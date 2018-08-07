1b5eb867a0193adcc52e3538eb1c664da10060f1
Erik Santiago <eriksantiago.science@gmail.com>
2018-08-07 15:34:21 +0700

    update data_akun view

--- application/views/pages/data_akun.php
+++ application/views/pages/data_akun.php
@@ -30,7 +30,6 @@
 	      <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">User</a>
 	      <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Supervisor</a>
 	      <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Admin</a>
-	      <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
 	    </div>
 	  </div>
 	  <div class="col-10 offset-2">
@@ -158,7 +157,6 @@
 				</table>
         	</div>
 	      </div>
-	      <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
 	    </div>
 	  </div>
 	</div>
